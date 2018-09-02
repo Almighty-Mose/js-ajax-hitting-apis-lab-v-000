443d95691f52619ee75b9aef056107fda02145f9
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-02 19:33:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const rootURL = 'https://api.github.com';
 
 function getRepositories() {
   let name = document.getElementById("username").value;
+  const uri = rootURL + '/users/' + name + '/repos';
   let req = new XMLHttpRequest();
   req.
 }
