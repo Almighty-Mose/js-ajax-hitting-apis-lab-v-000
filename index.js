473f97c373e070c8dbeef7f1441375a4ea954b0f
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-02 19:49:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ function showRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
 
-function getCommits() {
-  
+function getCommits(el) {
+  const name = el.dataset.
 }
+
+// function getCommits(el) {
+//   const name = el.dataset.repo
+//   const req = new XMLHttpRequest()
+//   req.addEventListener("load", showCommits)
+//   req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+//   req.send()
+// }
