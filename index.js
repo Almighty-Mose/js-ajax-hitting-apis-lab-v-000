e96e737f46afa4d606e417a5208dc837ebcb2be7
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-02 20:10:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,13 +59,21 @@ function getBranches(el) {
   req.send();
 }
 
+// function displayBranches() {
+//   const branches = JSON.parse(this.responseText);
+//   const branchesList =
+//     `<ul>${branches.map(branch =>
+//       '<li>' + branch.name + '</li>'
+//     )
+//     .join('')}</ul>`;
+  
+//   document.getElementById('details').innerHTML = branchesList;
+// }
+
 function displayBranches() {
   const branches = JSON.parse(this.responseText);
-  const branchesList =
-    `<ul>${branches.map(branch =>
-      '<li>' + branch.name + '</li>'
-    )
+  const branchesList = `<ul>${branches
+    .map(branch => '<li>' + branch.name + '</li>')
     .join('')}</ul>`;
-  
   document.getElementById('details').innerHTML = branchesList;
 }
