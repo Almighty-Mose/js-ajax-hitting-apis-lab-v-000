d64a4bc0bc1380c9b6d57911ffe056a12f4c0eb2
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-02 19:53:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,16 +30,17 @@ function showRepositories() {
 
 function getCommits(el) {
   const name = el.dataset.repository;
-  const uri = rootURL + '/commits'
+  const uri = rootURL + '/repos/' + el.dataset.username + '/' + repoName + '/commits';
   const req = new XMLHttpRequest();
-  req.addEventListener("load", showCommits);
-  req.open("GET", )
+  req.addEventListener("load", displayCommits);
+  req.open("GET", uri);
+  req.send();
 }
 
-// function getCommits(el) {
-//   const name = el.dataset.repo
-//   const req = new XMLHttpRequest()
-//   req.addEventListener("load", showCommits)
-//   req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
-//   req.send()
-// }
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = 
+    '<ul>' + commits.map(commit => {
+      
+    })
+}
